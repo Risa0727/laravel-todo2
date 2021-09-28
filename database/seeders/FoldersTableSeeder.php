e8890728f38f49651a89c7dd7db1bc195fcd9a3b
risarisarisa <from.aqua.to@gmail.com>
2021-09-28 15:02:43 +1000

    Added user id Added Seeder

--- database/seeders/FoldersTableSeeder.php
+++ database/seeders/FoldersTableSeeder.php
@@ -15,11 +15,13 @@ class FoldersTableSeeder extends Seeder
      */
     public function run()
     {
+        $user = DB::table('users')->first();
         $titles = ['private', 'work', 'travel'];
 
         foreach ($titles as $title) {
           DB::table('folders')->insert([
             'title' => $title,
+            'user_id' => $user->id,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
           ]);
