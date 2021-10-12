0a929b3308d3525965e7194339fdb9d0123dee59
risarisarisa <from.aqua.to@gmail.com>
2021-10-12 15:28:46 +1100

    Revert "[Bugfix] HTTP -> Http"
    This reverts commit 9a76ab251e2b5b1f02c4f23f28035873b21a0af0.

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Models\Folder;
-use App\Http\Requests\CreateFolder;
+use App\HTTP\Requests\CreateFolder;
 
 class FolderController extends Controller
 {
